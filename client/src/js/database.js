8544377829e9a3a257c2687486b48558335c153b
Matthew Tobin <tobin.matthew94@gmail.com>
2023-03-13 18:27:29 -0700

    added GET and PUT route

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,8 +16,8 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error('putDb not implemented');
   console.log('PUT to the database');
-  const todosDb = await openDB('jate', 1);
-  const tx = todosDb.transaction('jate', 'readwrite');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({ id: id, jate: content });
   const result = await request;
@@ -25,6 +25,16 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  console.log('GET all from database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+} 
 
 initdb();
